418df700a2fca953d7f53763f31f7cbcf395b946
MRFolio <roose.markus@gmail.com>
2021-04-23 21:39:43 +0300

    nodejs practice till http

--- 1-node-tutorial/10-fs-sync.js
+++ 1-node-tutorial/10-fs-sync.js
@@ -1,12 +1,17 @@
-const { readFileSync, writeFileSync } = require('fs')
-console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const { readFileSync, writeFileSync } = require('fs');
+console.log('start');
+
+const first2 = readFileSync('./content/first.txt', 'utf16le');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
 
 writeFileSync(
   './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`,
+  `\nHere is the result : \n${first} \r\n${second}`,
   { flag: 'a' }
-)
-console.log('done with this task')
-console.log('starting the next one')
+);
+console.log('done with this task');
+console.log('starting the next one');
+
+writeFileSync('./content/result-sync2.txt', `test: ${first2}`);
